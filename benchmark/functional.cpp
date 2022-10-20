39e291a79be1da0a2a199247face874d1c69dda8
edmondj <julien.edmond@epitech.eu>
2022-10-20 16:00:13 -0400

    cleaned up benchmarks

--- benchmark/functional.cpp
+++ benchmark/functional.cpp
@@ -2,106 +2,36 @@
 #include <functional>
 #include <alt_std/functional.h>
 
-static void std_ctor_fptr(benchmark::State& state) {
+
+template<template<typename> typename function>
+void ctor_fptr(benchmark::State& state) {
   for (auto _ : state) {
-    benchmark::DoNotOptimize(std::function<void()>([]() {}));
+    benchmark::DoNotOptimize(function<void()>([]() {}));
   }
 }
-BENCHMARK(std_ctor_fptr);
+BENCHMARK(ctor_fptr<std::function>);
+BENCHMARK(ctor_fptr<alt::function>);
 
-static void alt_ctor_fptr(benchmark::State& state) {
+template<template<typename> typename function, size_t size>
+void ctor_capture(benchmark::State& state) {
+  struct payload
+  {
+    char data[size];
+  };
+
   for (auto _ : state) {
-    benchmark::DoNotOptimize(alt::function<void()>([]() {}));
+    benchmark::DoNotOptimize(function<const char* ()>([answer = payload{}]() { return answer.data; }));
   }
 }
-BENCHMARK(alt_ctor_fptr);
-
-template<size_t Size>
-struct Payload
-{
-  char data[Size];
-};
-
-static void std_ctor_capture_8(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(std::function<const char* ()>([answer = Payload<8>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(std_ctor_capture_8);
-
-static void alt_ctor_capture_8(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(alt::function<const char* ()>([answer = Payload<8>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(alt_ctor_capture_8);
-
-static void std_ctor_capture_16(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(std::function<const char* ()>([answer = Payload<16>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(std_ctor_capture_16);
-
-static void alt_ctor_capture_16(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(alt::function<const char* ()>([answer = Payload<16>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(alt_ctor_capture_16);
-
-static void std_ctor_capture_32(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(std::function<const char* ()>([answer = Payload<32>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(std_ctor_capture_32);
-
-static void alt_ctor_capture_32(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(alt::function<const char* ()>([answer = Payload<32>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(alt_ctor_capture_32);
-
-static void std_ctor_capture_64(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(std::function<const char* ()>([answer = Payload<64>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(std_ctor_capture_64);
-
-static void alt_ctor_capture_64(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(alt::function<const char* ()>([answer = Payload<64>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(alt_ctor_capture_64);
-
-static void std_ctor_capture_128(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(std::function<const char* ()>([answer = Payload<128>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(std_ctor_capture_128);
-
-static void alt_ctor_capture_128(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(alt::function<const char* ()>([answer = Payload<128>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(alt_ctor_capture_128);
-
-static void std_ctor_capture_256(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(std::function<const char*()>([answer = Payload<256>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(std_ctor_capture_256);
-
-static void alt_ctor_capture_256(benchmark::State& state) {
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(alt::function<const char* ()>([answer = Payload<256>{}]() { return answer.data; }));
-  }
-}
-BENCHMARK(alt_ctor_capture_256);
+BENCHMARK(ctor_capture<std::function, 8>);
+BENCHMARK(ctor_capture<alt::function, 8>);
+BENCHMARK(ctor_capture<std::function, 16>);
+BENCHMARK(ctor_capture<alt::function, 16>);
+BENCHMARK(ctor_capture<std::function, 32>);
+BENCHMARK(ctor_capture<alt::function, 32>);
+BENCHMARK(ctor_capture<std::function, 64>);
+BENCHMARK(ctor_capture<alt::function, 64>);
+BENCHMARK(ctor_capture<std::function, 128>);
+BENCHMARK(ctor_capture<alt::function, 128>);
+BENCHMARK(ctor_capture<std::function, 256>);
+BENCHMARK(ctor_capture<alt::function, 256>);
