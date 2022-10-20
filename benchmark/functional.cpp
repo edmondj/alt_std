2dff59b2d6e70db2b96860d1450a989b99972555
edmondj <julien.edmond@epitech.eu>
2022-10-20 16:16:41 -0400

    Added invoke benchmarks

--- benchmark/functional.cpp
+++ benchmark/functional.cpp
@@ -35,3 +35,40 @@ BENCHMARK(ctor_capture<std::function, 128>);
 BENCHMARK(ctor_capture<alt::function, 128>);
 BENCHMARK(ctor_capture<std::function, 256>);
 BENCHMARK(ctor_capture<alt::function, 256>);
+
+template<template<typename> typename function>
+void invoke_fptr(benchmark::State& state)
+{
+  function<int(int, int)> f = [](int l, int r) { return l + r; };
+  for (auto _ : state)
+  {
+    benchmark::DoNotOptimize(f(1, 2));
+  }
+}
+BENCHMARK(invoke_fptr<std::function>);
+BENCHMARK(invoke_fptr<alt::function>);
+
+template<template<typename> typename function>
+void invoke_sbo(benchmark::State& state)
+{
+  function<int(int, int)> f = [i = 42](int l, int r) { return l + r + i; };
+  for (auto _ : state)
+  {
+    benchmark::DoNotOptimize(f(1, 2));
+  }
+}
+BENCHMARK(invoke_sbo<std::function>);
+BENCHMARK(invoke_sbo<alt::function>);
+
+template<template<typename> typename function>
+void invoke_heavy(benchmark::State& state)
+{
+  char payload[256]{};
+  function<int(int, int)> f = [payload](int l, int r) { return l + r; };
+  for (auto _ : state)
+  {
+    benchmark::DoNotOptimize(f(1, 2));
+  }
+}
+BENCHMARK(invoke_heavy<std::function>);
+BENCHMARK(invoke_heavy<alt::function>);
